4afd86d2d7149d5ca0c8a7de59f5d26b6544bf7e
mohsin19295 <mohsin19295@gmail.com>
2022-07-09 15:50:22 +0530

    filter range value set

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const displayImg = document.querySelector(".display-img img");
 const mainContainer = document.querySelector(".main-container");
 const filterButtons = document.querySelectorAll(".filter button");
 const inputRange = document.querySelector(".slider input");
+const inputValue = document.querySelector(".slider .value");
+
+let [brightness, saturation, inversion, grayscale] = [100, 100, 0, 0]
 
 const uploadImg = () => {
   let file = fileUpload.files[0]; // Get selected file
@@ -20,11 +23,44 @@ filterButtons.forEach((select => {
       document.querySelector(".filter .active").classList.remove("active");
       select.classList.add("active");
       document.querySelector(".slider .name").innerHTML = select.innerHTML; // Passing filter name
+
+      // Set indivisual selected filter value
+         if (select.id === "brightness") {
+           inputRange.max = "200";
+           inputRange.value = brightness;
+           inputValue.innerHTML = `${brightness}%`;
+         } else if (select.id === "saturation") {
+           inputRange.max = "200";
+           inputRange.value = saturation;
+           inputValue.innerHTML = `${saturation}%`;
+         } else if (select.id === "inversion") {
+           inputRange.max = "100";
+           inputRange.value = inversion;
+           inputValue.innerHTML = `${inversion}%`;
+         } else {
+           inputRange.max = "100";
+           inputRange.value = grayscale;
+           inputValue.innerHTML = `${grayscale}%`;
+         }
     })
 }))
 
 const displayRangeValue = () => {
-  document.querySelector(".slider .value").innerHTML = `${inputRange.value}%`; // Passing inputRage
+  inputValue.innerHTML = `${inputRange.value}%`; // Passing inputRage
+
+  // Set selected filter range
+    let filterId = document.querySelector(".filter .active").id;
+    console.log(inputRange.value);
+
+  if (filterId === "brightness") {
+    brightness = inputRange.value;
+  } else if (filterId === "saturation") {
+    saturation = inputRange.value;
+  } else if (filterId === "inversion") {
+    inversion = inputRange.value;
+  } else {
+    grayscale = inputRange.value;
+  }
 }
 
 inputRange.addEventListener('input', displayRangeValue)
